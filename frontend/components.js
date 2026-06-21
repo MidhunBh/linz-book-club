63f840016c037c186299005414304f98f6dbf021
MidhunBh <midhunbh45@gmail.com>
2026-06-21 22:49:46 +0200

    merge meetings into admin, remove profile and meetings pages

--- frontend/components.js
+++ frontend/components.js
@@ -6,7 +6,6 @@ const AppNav = {
         <li><a href="index.html"     :class="['nav-link', { active: page === 'home' }]">Home</a></li>
         <li><a href="maingroup.html" :class="['nav-link', { active: page === 'main' }]">Main Group</a></li>
         <li><a href="fantasy.html"   :class="['nav-link', { active: page === 'fantasy' }]">Fantasy Group</a></li>
-        <li><a href="meetings.html"  :class="['nav-link', { active: page === 'meetings' }]">Meetings</a></li>
         <li><a href="forum.html"     :class="['nav-link', { active: page === 'forum' }]">Forum</a></li>
         <li><a href="admin.html"     :class="['nav-link', { active: page === 'admin' }]">Organizer</a></li>
         <li><a href="login.html"     :class="['nav-link', { active: page === 'login' }]">Login</a></li>
